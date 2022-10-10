31c0cb6352f09c7a24ae36febae9496b402c39d2
pinglee0 <lzn90@outlook.com>
2022-10-10 08:02:46 +0800

    1

--- uinit4/lesson.java
+++ uinit4/lesson.java
@@ -125,3 +125,17 @@ class example2{
         }
     }
 }
+class example3{
+    public static void main(String[] args){
+        int i=1;
+        for(i=1;i<5;i++){
+
+        }
+        System.out.println(i);
+        for(int j=1;j<5;j++){
+
+        }
+        //System.out.println(j);
+
+    }
+}
